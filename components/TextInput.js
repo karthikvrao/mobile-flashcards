3c4691e5b1701aee33517c6c94c83c96760f7d89
Karthik Rao <karthikvrao88@gmail.com>
2018-04-19 17:50:02 +0530

    feat: Create custom TextInput component

---
+++ components/TextInput.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+
+const TextInput = styled.TextInput`
+  align-self: stretch;
+  height: 48;
+  border-color: black;
+  border-radius: 5;
+  border-width: 2;
+  margin: 40px 0;
+  padding: 8px;
+`;
+
+export default TextInput;
