33eb9cdadbe403a5dd36933e0f8109837a481e11
Karthik Rao <karthikvrao88@gmail.com>
2018-05-02 20:56:47 +0530

    feat: Implement methods for various AsyncStorage related activities

---
+++ utils/storage.js
@@ -0,0 +1,37 @@
+import { AsyncStorage } from 'react-native';
+
+const DECKS_STORAGE_KEY = 'MobileFlashcards:Decks';
+
+const getDecks = () => (
+  AsyncStorage.getItem(DECKS_STORAGE_KEY).then(results => JSON.parse(results))
+);
+
+const getDeck = title => (
+  AsyncStorage.getItem(DECKS_STORAGE_KEY).then(result => JSON.parse(result[title]))
+);
+
+const saveDeckTitle = title =>
+  AsyncStorage.getItem(DECKS_STORAGE_KEY).then((result) => {
+    if (result === null) {
+      return AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify({
+        [title]: { title, questions: [] },
+      }));
+    }
+
+    const data = JSON.parse(result);
+    return AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify({
+      ...data,
+      [title]: { title, questions: [] },
+    }));
+  });
+
+const addCardToDeck = (title, card) => {
+  AsyncStorage.getItem(DECKS_STORAGE_KEY).then((result) => {
+    const data = JSON.parse(result);
+    // Assuming that a deck with given title exists
+    data[title].questions = data[title].questions.concat([].push(card));
+    return AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(data));
+  });
+};
+
+export { getDecks, getDeck, saveDeckTitle, addCardToDeck };
